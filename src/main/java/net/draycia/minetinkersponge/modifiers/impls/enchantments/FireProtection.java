61a0820b4ee0f0ec47d0e2cf29d258b6d0d9e9c0
Draycia <lonelyyordle@gmail.com>
2019-09-05 23:01:13 -0700

    Make protection mods incompatible with each other

--- src/main/java/net/draycia/minetinkersponge/modifiers/impls/enchantments/FireProtection.java
+++ src/main/java/net/draycia/minetinkersponge/modifiers/impls/enchantments/FireProtection.java
@@ -8,10 +8,23 @@ import org.spongepowered.api.item.ItemTypes;
 import org.spongepowered.api.item.enchantment.EnchantmentType;
 import org.spongepowered.api.item.enchantment.EnchantmentTypes;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class FireProtection extends Modifier {
+
+    private static List<Class<? extends Modifier>> incompatibleTypes = new ArrayList<>();
+
+    static {
+        incompatibleTypes.add(Protection.class);
+        incompatibleTypes.add(ProjectileProtection.class);
+    }
+
+    @Override
+    public List<Class<? extends Modifier>> getIncompatibleModifiers() {
+        return incompatibleTypes;
+    }
 
     @Override
     public String getName() {
