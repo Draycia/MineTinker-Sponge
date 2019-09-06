07bdc025a046b3c0c33b2301c5063682a99e7720
Draycia <lonelyyordle@gmail.com>
2019-09-05 23:03:38 -0700

    Make damage mods incompatible with each other

--- src/main/java/net/draycia/minetinkersponge/modifiers/impls/enchantments/BaneOfArthropods.java
+++ src/main/java/net/draycia/minetinkersponge/modifiers/impls/enchantments/BaneOfArthropods.java
@@ -8,10 +8,23 @@ import org.spongepowered.api.item.ItemTypes;
 import org.spongepowered.api.item.enchantment.EnchantmentType;
 import org.spongepowered.api.item.enchantment.EnchantmentTypes;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class BaneOfArthropods extends Modifier {
+
+    private static List<Class<? extends Modifier>> incompatibleTypes = new ArrayList<>();
+
+    static {
+        incompatibleTypes.add(Sharpness.class);
+        incompatibleTypes.add(Smite.class);
+    }
+
+    @Override
+    public List<Class<? extends Modifier>> getIncompatibleModifiers() {
+        return incompatibleTypes;
+    }
 
     @Override
     public String getName() {
