e4d4b448924889d65b5ba9e0b4a659ae3c085d30
Draycia <lonelyyordle@gmail.com>
2020-04-25 07:13:59 -0700

    Fix enchanted book conversion (not recipes)

--- src/main/java/net/draycia/minetinkersponge/listeners/InteractListener.java
+++ src/main/java/net/draycia/minetinkersponge/listeners/InteractListener.java
@@ -13,6 +13,8 @@ import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.block.InteractBlockEvent;
 import org.spongepowered.api.event.cause.EventContextKeys;
+import org.spongepowered.api.event.filter.IsCancelled;
+import org.spongepowered.api.event.filter.cause.First;
 import org.spongepowered.api.event.filter.cause.Root;
 import org.spongepowered.api.item.ItemTypes;
 import org.spongepowered.api.item.enchantment.Enchantment;
@@ -30,7 +32,7 @@ import java.util.List;
 public class InteractListener {
 
     @Listener
-    public void onBookConvert(InteractBlockEvent.Secondary event, @Root Player player) {
+    public void onBookConvert(InteractBlockEvent.Secondary.OffHand event, @Root Player player) {
         // Ensure the player has permission to convert enchantments to modifiers
         if (!player.hasPermission("minetinker.convertenchants")) {
             return;
@@ -115,6 +117,64 @@ public class InteractListener {
                             player.setItemInHand(HandTypes.OFF_HAND, result.getItemStack());
                         }
                     });
+                }
+            });
+        });
+
+        // Ensure the player has permission to convert enchantments to modifiers
+        if (!player.hasPermission("minetinker.convertenchants")) {
+            return;
+        }
+
+        // Check if the interacted block is the one used for conversion
+        if (event.getTargetBlock().getState().getType() != MTConfig.ENCHANTMENT_CONVERT_BLOCK) {
+            return;
+        }
+
+        // Get the item used in the event
+        player.getItemInHand(event.getHandType()).ifPresent(itemStack -> {
+            // Check if the item is an enchanted book
+            if (itemStack.getType() != ItemTypes.ENCHANTED_BOOK) {
+                return;
+            }
+
+            itemStack.get(Keys.STORED_ENCHANTMENTS).ifPresent(enchantments -> {
+                List<Enchantment> enchantsToRemove = new ArrayList<>();
+
+                // Loop through all enchantments the book has
+                for (Enchantment enchantment : enchantments) {
+                    // Try to find any modifier that applies the enchantment
+                    ModManager.getFirstModifierByEnchantment(enchantment.getType()).ifPresent(modifier -> {
+                        // Give the player the modifier
+                        ItemStack modifierItem = modifier.getModifierItem(enchantment.getLevel());
+
+                        // If the player can fit the modifier in their inventory, give them it directly
+                        Inventory inventory = player.getInventory()
+                                .query(QueryOperationTypes.INVENTORY_TYPE.of(MainPlayerInventory.class));
+
+                        if (inventory.canFit(modifierItem)) {
+                            inventory.offer(modifierItem);
+                        } else {
+                            // If the player can't fit the item in their inventory, drop it next to them
+                            Location<World> location = player.getLocation();
+
+                            Entity itemEntity = location.createEntity(EntityTypes.ITEM);
+                            itemEntity.offer(Keys.ACTIVE_ITEM, modifierItem.createSnapshot());
+
+                            location.spawnEntity(itemEntity);
+                        }
+
+                        // Remove the enchantment from the item
+                        enchantsToRemove.add(enchantment);
+                    });
+                }
+
+                enchantments.removeAll(enchantsToRemove);
+
+                if (enchantments.isEmpty()) {
+                    player.setItemInHand(event.getHandType(), ItemStack.empty());
+                } else {
+                    itemStack.offer(Keys.STORED_ENCHANTMENTS, enchantments);
                 }
             });
         });
